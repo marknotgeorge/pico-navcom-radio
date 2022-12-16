ee7455404dc7e8834a9fa25baafd073a997f4f4d
Mark Johnson <marknotgeorge90@hotmail.com>
2022-12-16 13:17:23 +0000

    Add TinyUsb CDC code

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,48 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
+#include "bsp/board.h"
+
+#include "tusb.h"
+
 #define ONBOARD_LED 25
+
+void cdc_task(void);
+void parse_cdc_input(char buf[], uint32_t count);
 
 int main()
 {
+    board_init();
     stdio_init_all();
-    printf("Simple C/CPP Template \n");
+    tusb_init();
     gpio_init(ONBOARD_LED);
     gpio_set_dir(ONBOARD_LED, GPIO_OUT);
     
     while (1)
     {
-        gpio_put(ONBOARD_LED, 0);
-        sleep_ms(800);
-        gpio_put(ONBOARD_LED, 1);
-        sleep_ms(200);
+        tud_task();
+        cdc_task();
     }
 }
+
+void cdc_task(void)
+{
+    if (tud_cdc_available())
+    {
+        char buf[64];
+        uint32_t count = tud_cdc_read(buf, sizeof(buf));       
+
+        parse_cdc_input(buf, count);
+    }
+}
+
+void parse_cdc_input(char buf[], uint32_t count)
+{
+    printf("Received %d bytes.\r\n", count);
+    printf("Received: %s", buf);
+    
+}
+
+
+
+
